8baccf48f902f1c9be45f761363f4480854816cc
Javicho <javierguti619@gmail.com>
2020-10-19 17:48:33 -0500

    add docs

---
+++ docs/precache-manifest.5e8344d3b9ee5de50e5d5e0b98ab581a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74cff81932389fcd07ceadc4a3e64802",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bdcaa65bb029b6c337c",
+    "url": "/static/css/main.5bfeecbf.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bdcaa65bb029b6c337c",
+    "url": "/static/js/main.49267477.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
